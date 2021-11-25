093076bdd85542c3487061c89a1b7045b8c279f8
ksuess <k.suess@rohberg.ch>
2021-11-25 02:33:46 +0100

    Update PDBView

--- src/rohberg/volto/browser/views.py
+++ src/rohberg/volto/browser/views.py
@@ -6,6 +6,9 @@ logger = logging.getLogger("views of rohberg.igib")
 
 class PDBView(BrowserView):
     def __call__(self):
+        context = self.context
+        ctx = context.aq_base
+
         import pdb; pdb.set_trace()
 
         return 'pdb view done.'
