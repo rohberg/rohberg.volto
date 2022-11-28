d16701fa6ea88531a447fd172d01810503154543
Katja Süss <k.suess@rohberg.ch>
2022-11-28 20:08:05 +0100

    Fiddle PDBView

--- src/rohberg/volto/browser/views.py
+++ src/rohberg/volto/browser/views.py
@@ -1,3 +1,5 @@
+from importlib import import_module
+
 from Products.Five import BrowserView
 import logging
 
@@ -9,6 +11,13 @@ class PDBView(BrowserView):
         context = self.context
         ctx = context.aq_base
 
-        import pdb; pdb.set_trace()
+        PLONE_6 = getattr(
+            import_module("Products.CMFPlone.factory"), "PLONE60MARKER", False
+        )
+        print("PLONE_6", PLONE_6)
 
-        return 'pdb view done.'
+        import pdb
+
+        pdb.set_trace()
+
+        return "pdb view done."
